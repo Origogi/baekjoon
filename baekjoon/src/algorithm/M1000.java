4340118a52de423118837a212c0dfb8a083bb73f
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 16:54:42 +0900

    1000. A+B

--- baekjoon/src/algorithm/M1000.java
+++ baekjoon/src/algorithm/M1000.java
@@ -2,7 +2,7 @@ import java.util.*;
 //1000. A+B
 // https://www.acmicpc.net/problem/1000
 
-public class Main {
+public class M1000 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
