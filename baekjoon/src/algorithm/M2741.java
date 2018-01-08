c21c8b01ff53d2b0d5648d74299301e3808d61f6
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 17:45:15 +0900

    2741. N 찍기

---
+++ baekjoon/src/algorithm/M2741.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//2741. N 찍기
+//https://www.acmicpc.net/problem/2741
+
+public class M2741 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        for (int i = 1;i<=n;i++)
+            System.out.println(i);
+    }
+}
