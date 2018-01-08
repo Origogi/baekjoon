4b8ecdf23c76ae5882b2f976f4122251fd69d72a
Administrator <Administrator@MSDN-SPECIAL>
2018-01-09 01:12:01 +0900

    .

---
+++ baekjoon/src/chapter2/M1001.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class M1001 {
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+
+        int a;
+        int b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println("" + (a-b));
+	}
+}
---
+++ baekjoon/src/chapter3/M11721.java
@@ -0,0 +1,23 @@
+package chapter3;
+
+import java.util.Scanner;
+
+//11721. 열 개씩 끊어 출력하기 
+//https://www.acmicpc.net/problem/11721
+
+public class M11721 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.next();
+		scan.close();
+		
+		for(int i = 0;i<input.length();i++) {
+			System.out.print(input.charAt(i));
+			if (0!=i && 0== (i+1)%10) {
+				System.out.println();
+			}
+		}
+	}
+
+}
