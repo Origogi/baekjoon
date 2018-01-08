dd7871f13186eda3e452eb7610e0edd27a3d54b1
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 16:52:43 +0900

    1000. A+B

---
+++ baekjoon/src/algorithm/M1000.java
@@ -0,0 +1,12 @@
+import java.util.*;
+//1000. A+B
+// https://www.acmicpc.net/problem/1000
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextInt() + scan.nextInt());
+        scan.close();
+    }
+}
