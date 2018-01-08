4a1018e7e3eb92a72f52b321db1e822029b7d6a8
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 16:56:55 +0900

    2557. Hello world

--- baekjoon/src/algorithm/M2557.java
+++ baekjoon/src/algorithm/M2557.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 // 2557. Hello world
 //https://www.acmicpc.net/problem/2557
 
-public class Main {
+public class M2557 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 	}
