f42a8049cc76b2478727c77c4e40d1b7204a14a2
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 18:02:05 +0900

    2439. 별 찍기

---
+++ baekjoon/src/algorithm/M2439.java
@@ -0,0 +1,23 @@
+// 2439. 별 찍기
+// https://www.acmicpc.net/problem/2439
+
+import java.util.*;
+
+
+public class M2439 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int h = scan.nextInt();
+        scan.close();
+        for (int i = 1;i<=h;i ++) {
+            for (int j = (h- i);j > 0;j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <=i ;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
