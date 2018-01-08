27a68fe2fcb05f8b3b30b924b64d47dbf33a90f1
Origogi <35194820+Origogi@users.noreply.github.com>
2018-01-08 17:44:13 +0900

    2742. 기찍N

---
+++ baekjoon/src/algorithm/M2742.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+//2742. 기찍 N
+//https://www.acmicpc.net/problem/27412
+
+public class M2742 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        for (int i = n;i>0;i--)
+            System.out.println(i);
+    }
+}
